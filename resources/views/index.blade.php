c246983538c2ca2dcfbd87fbd2c6252711b5337a
Chris Browder <rcbrowder@gmail.com>
2018-06-29 15:44:54 -0400

    Now adds index view and UsersTableSeeder.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Fantasy Baseball Player Ranker</title>
+        <!-- Adds Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    </head>
+    <body class="container">
+
+        
+
+    </body>
+</html>
