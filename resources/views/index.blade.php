bc310d1fb026ac0575fc22e2a14fca2ade8362f2
Chris Browder <rcbrowder@gmail.com>
2018-07-03 11:36:57 -0400

    Adds background images, formats columns, and implements zoom on hover.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,50 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
     <head>
+
         <meta charset="utf-8">
         <title>Fantasy Baseball Player Ranker</title>
         <!-- Adds Bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-
         <style>
-            .container-fluid {
-                /* display: flex;
-                height: 100%; */
-            }
-            #batterCol {
-                background-color: blue;
-                /* height: 100% !important; */
-            }
-            #pitcherCol {
-                background-color: red;
-                /* height: 100% !important; */
-
-            }
-            .row {
-                /* height: 100%; */
-            }
-            .col {
-                /* flex-grow: 1 1; */
+            .grow:hover
+            {
+            -webkit-transform: scale(1.3);
+            -ms-transform: scale(1.3);
+            transform: scale(1.3);
+            transition-duration: .3s;
             }
 
-            body {
-                /* height: 100vh !important; */
+            .outerdiv {
+                overflow: hidden;
+                min-width: 400px !important;
             }
+
+            #batterBG {
+                width: 100%;
+                height: 100%;
+                background-image: url(https://cdn-images-1.medium.com/max/1280/1*luSmy-qRh-0Ij5XfMhFjsw.jpeg);
+                background-size: cover;
+                background-position: 20%;
+                transition: all 1s ease;
+            }
+
+            #pitcherBG {
+                width: 100%;
+                height: 100%;
+                background-image: url(https://cdn-s3.si.com/styles/marquee_large_2x/s3/images/GettyImages-866042018.jpg?itok=yNdOvOWi);
+                background-size: cover;
+                background-position: center;
+                transition: all 1s ease;
+            }
+
 
         </style>
+
     </head>
 
-    <body class="container-fluid">
+    <body class="container-fluid h-100 p-0">
 
-        <div class="row flex-fill">
+        <div class="d-flex flex-wrap h-100">
 
-            <div id="batterCol" class="col-sm h-100">
-                <p>Batter</p>
+            <div id="batterCol" class="flex-fill outerdiv">
+                <div id="batterBG" class="grow">
+                </div>
             </div>
 
-            <div id="pitcherCol" class="col-sm h-100">
-                <p>Pitcher</p>
+            <div id="pitcherCol" class="flex-fill outerdiv">
+                <div id="pitcherBG" class="grow">
+                </div>
             </div>
         </div>
 
