faa24a319b51574d757f00dcc2ad5cf72c98c5de
Chris Browder <rcbrowder@gmail.com>
2018-07-11 16:50:17 -0400

    Fix bootstrap dependancy in package.json.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
