3f7c862de1440d1f1453705d504f9bddc4400f54
Chris Browder <rcbrowder@gmail.com>
2018-08-01 09:55:47 -0400

    Seeders running successfully.

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -14,35 +14,36 @@ class CategoriesTableSeeder extends Seeder
     public function run() {
 
         $categories = [
-            '1B': 'Singles',
-            '2B': 'Doubles',
-            '3B': 'Triples',
-            'HR': 'Home Runs',
-            'BA': 'Batting Average',
-            'BB': 'Walks',
-            'XBH': 'Extra Base Hits',
-            'H': 'Extra Base Hits',
-            'HBP': 'Hit By Pitchs',
-            'K': 'Strikeouts',
-            'OBP': 'On-Base Percentage',
-            'OPS': 'On-Base Plus Slugging',
-            'R': 'Runs',
-            'RBI': 'Runs Batted In',
-            'SLG': 'Slugging Percentage',
-            'SB': 'Stolen Bases',
-            'TB': 'Total Bases',
-            'ISO': 'Isolated Power',
-            'WAR': 'Wins Above Replacement',
-            'G': 'Games Played'
-        ]};
+            '1B' => 'Singles',
+            '2B' => 'Doubles',
+            '3B' => 'Triples',
+            'HR' => 'Home Runs',
+            'BA' => 'Batting Average',
+            'BB' => 'Walks',
+            'XBH' => 'Extra Base Hits',
+            'H' => 'Extra Base Hits',
+            'HBP' => 'Hit By Pitchs',
+            'K' => 'Strikeouts',
+            'OBP' => 'On-Base Percentage',
+            'OPS' => 'On-Base Plus Slugging',
+            'R' => 'Runs',
+            'RBI' => 'Runs Batted In',
+            'SLG' => 'Slugging Percentage',
+            'SB' => 'Stolen Bases',
+            'TB' => 'Total Bases',
+            'ISO' => 'Isolated Power',
+            'WAR' => 'Wins Above Replacement',
+            'G' => 'Games Played'
+        ];
 
-    foreach ($categories as $abv => $name) {
+        foreach ($categories as $abv => $name) {
 
-        DB::table('categories')->insert([
-            'name' => $name,
-            'abreviation' => $abv,
-            'created_at' => Carbon::now(),
-    		'updated_at' => Carbon::now()
-        ]);
+            DB::table('categories')->insert([
+                'name' => $name,
+                'abreviation' => $abv,
+                'created_at' => Carbon::now(),
+        		'updated_at' => Carbon::now()
+            ]);
+        }
     }
 }
