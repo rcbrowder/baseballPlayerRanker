4058ba853a8d60c4da4b3e1b85a521aae3f50c3c
Chris Browder <rcbrowder@gmail.com>
2018-08-01 09:14:36 -0400

    Updates UsersTableSeeder. Adds 'nullable' modifier to a couple of columns in users_table migration.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -9,8 +9,26 @@ class UsersTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+
+        DB::table('users')->insert([
+    		'name' => 'Chris',
+    		'email' => 'chris@example.com',
+    		'password' => bcrypt('chris'),
+            'batterCategories' => null,
+            'pitcherCategories' => null,
+    		'created_at' => Carbon::now(),
+    		'updated_at' => Carbon::now()
+    	]);
+
+        DB::table('users')->insert([
+    		'name' => 'Cameron',
+    		'email' => 'Cameron@example.com',
+    		'password' => bcrypt('cameron'),
+            'batterCategories' => null,
+            'pitcherCategories' => null,
+    		'created_at' => Carbon::now(),
+    		'updated_at' => Carbon::now()
+    	]);
     }
 }
