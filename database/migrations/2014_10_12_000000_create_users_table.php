1632b40b09d10853b8d30310577de910a8e3dd4f
Chris Browder <rcbrowder@gmail.com>
2018-08-01 08:39:05 -0400

    Adds batterCategories and pitcherCategories as columns in users table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('batterCategories', 20);
+            $table->string('pitcherCategories', 20);
             $table->rememberToken();
             $table->timestamps();
         });
