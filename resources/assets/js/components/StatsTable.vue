9bef35d00c33661ee573c8a0ba5ad31027323bbb
Chris Browder <rcbrowder@gmail.com>
2018-08-03 08:23:59 -0400

    Created StatsTable.vue and added it to app.js.

---
+++ resources/assets/js/components/StatsTable.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <table class="table">
+        <thead>
+            <tr class="font-weight-bold">
+                <td>Name</td>
+                <td>Position</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="stat in stats">
+                <td>{{ stat.name }}</td>
+                <td>{{ stat.position }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+</template>
+
+
+<script>
+    export default {
+        props: ['stats'],
+
+        data: () => ({
+
+        }),
+    }
+</script>
