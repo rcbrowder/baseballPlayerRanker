fa2a244233daf7cc1e2dc3ebd346f4fb38f9c340
Chris Browder <rcbrowder@gmail.com>
2018-08-08 08:32:41 -0400

    Removed collapse from navbar so 'Login' and 'Register' are always showing.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,11 +33,8 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                      <i class="far fa-caret-square-down"></i>
-                    </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="navbar-expand" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
 
